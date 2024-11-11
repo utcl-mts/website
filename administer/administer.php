48bf5cfe6f339f82f66948e23b36d228ac713e4b
Alexia <alexiabaxter07@gmail.com>
2024-11-11 13:52:12 +0000

    updated: added basic php for adding info into administer table

--- administer/administer.php
+++ administer/administer.php
@@ -1,5 +1,25 @@
 <?php
 
+    // session start to be able to use session variables
+    session_start();
 
+    //include database connection
+    include '../database/database-2024-11-07.sql';
+
+    //gets info from form on html page
+    $dose = $_POST['dose'];
+    $staff_code = $_POST['staff_code'];
+    $time = $_POST['time'];
+
+    //puts informaition into administer database
+    $sql = "INSERT INTO administer (staff_code,date-time,dose_given) VALUES (?,?,?)";
+    $stmt = $conn->prepare($sql);
+
+    //bind parameters so that it is more secure of passing the informaitiomn
+    $stmt->bindParam(1, $staff_code);
+    $stmt->bindParam(2, $time);
+    $stmt->bindParam(3, $dose);
+
+    $stmt->execute();
 
 ?>
