62ff25ab464af25c60ec8c83275a39f35f0f4ea5
James <barowikajames@gmail.com>
2025-01-14 02:02:09 +0000

    styled: choose student

--- log/choose_student.php
+++ log/choose_student.php
@@ -13,37 +13,29 @@
     include '../server/db_connect.php';
 
 ?>
+<!DOCTYPE html>
 <html lang="en">
-
-    <head>
-
-        <meta charset="UTF-8">
-        <title>Administer Medicaition</title>
-        <link rel="stylesheet" href="/style.css">
-
-    </head>
-
-    <body>
-
-        <div class="container">
-
-            <!-- universal nav bar-->
-            <div class="navbar">
-
-                <img id="logo" src="../assets/images/utcleeds.svg" alt="UTC Leeds">
-
-                <h1 id="med_tracker">Med Tracker</h1>
-
-                <ul>
-
-                    <li><a href="../dashboard/dashboard.php">Home</a></li>
-                    <li><a href="../insert_data/insert_data_home.php">Insert Data</a></li>
-                    <li><a href="../bigtable/bigtable.php">Student Medication</a></li>
-                    <li><a href="administer.html">Log Medication</a></li>
-                    <li><a href="../whole_school/whole_school.php">Whole School Medication</a></li>
-                    <li class="logout"><a>Logout</a></li>
-
-                </ul>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/style/style.css">
+    <title>Insert Data</title>
+</head>
+<body class="full_page_styling">
+<div>
+    <div>
+        <ul class="nav_bar">
+            <div class="nav_left">
+                <li class="navbar_li"><a href="../dashboard/dashboard.php">Home</a></li>
+                <li class="navbar_li"><a href="../insert_data/insert_data_home.php">Insert Data</a></li>
+                <li class="navbar_li"><a href="../bigtable/bigtable.php">Student Medication</a></li>
+                <li class="navbar_li"><a href="../administer/administer.html">Administer Medication</a></li>
+                <li class="navbar_li"><a href="../whole_school/whole_school.php">Whole School Medication</a></li>
+            </div>
+            <div class="nav_left">
+                <li class="navbar_li"><a href="../logout.php">Logout</a></li>
+            </div>
+        </ul>
 
             </div>
 
@@ -66,22 +58,22 @@
 
                     echo "<form action='add_notes.php' method='POST'>";
 
-                        echo "<table>";
-
+                        echo "<table class='administer_choose_student'>";
+                        echo "<h1>Select Student</h1>";
                         // Display each student with a checkbox for selection
                         foreach ($result as $row) {
 
                             echo "<tr>";
                             echo "<td><input type='hidden' name='sid' value='" . $row['student_id'] . "'></td>";
-                            echo "<td><input type='checkbox' name='selected_students[]' value='" . $row['student_id'] . "'></td>";
-                            echo "<td>First name: " . htmlspecialchars($row['first_name']) . "</td><br>";
-                            echo "<td>Last name: " . htmlspecialchars($row['last_name']) . "</td>";
+                            echo "<td class='administer_choose_student_td'><input type='checkbox' name='selected_students[]' value='" . $row['student_id'] . "'></td>";
+                            echo "<td class='administer_choose_student_td'><b>First name: </b>" . htmlspecialchars($row['first_name']) . "</td><br>";
+                            echo "<td class='administer_choose_student_td'><b>Last name: </b>" . htmlspecialchars($row['last_name']) . "</td>";
                         }
 
                         // Submit button for the form
                         echo "</table>";
-
-                    echo "<input type='submit' name='submit' value='Select Students'>";
+                    echo "<br>";
+                    echo "<input class='submit' type='submit' name='submit' value='Select Students'>";
 
                     echo "</form>";
 
