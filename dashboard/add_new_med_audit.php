b3ac6ce5fcb284b303a9e524a44c9acf5acab99b
Alexia <161457494+alexbaxter07@users.noreply.github.com>
2025-01-14 19:35:29 +0000

    Updated: created the start of page but have not finished as of yet

--- dashboard/add_new_med_audit.php
+++ dashboard/add_new_med_audit.php
@@ -1,8 +1,8 @@
 <?php
 
-// Start a new session
-session_start();
-// Include the database connection file
-include "../server/db_connect.php";
+    // Start a new session
+    session_start();
+    // Include the database connection file
+    include "../server/db_connect.php";
 
 ?>
