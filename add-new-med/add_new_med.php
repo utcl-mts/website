4d3e29333c5afbf99d4bd84380f1fb0a0eadf6d1
Alexia <161457494+alexbaxter07@users.noreply.github.com>
2025-01-16 18:13:22 +0000

    updated: code now works

--- add-new-med/add_new_med.php
+++ add-new-med/add_new_med.php
@@ -2,12 +2,43 @@
 
 ##TODO
 
+    // Check if form is submitted
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        if (!empty($_POST['medication'])) {
+
+            try {
+
+                $sql = "INSERT INTO med (medication_name) VALUES (?)";
+                $stmt = $conn->prepare($sql);
+                $stmt->bindParam(1, $_POST['medication'], PDO::PARAM_STR);
+                $stmt->execute();
+                echo "Medication successfully added!";
+
+            } catch (PDOException $e) {
+
+            echo "Error: " . $e->getMessage();
+
+            }
+        } else {
+
+        echo "Please fill in the medication name.";
+
+        }
+
+    }
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
-    <link rel="stylesheet" href="../style.css">
+    <head>
+
+        <link rel="stylesheet" href="../style.css">
+        <title>Med Tracker</title>
+
+    </head>
 
     <body>
 
@@ -16,6 +47,7 @@
             <div class="navbar">
 
                 <img id="logo" src="../assets/UTCLeeds.svg" alt="UTC Leeds">
+
                 <h1 id="med_tracker">Med Tracker</h1>
 
                 <ul>
@@ -25,40 +57,30 @@
                     <li><a href="../bigtable/bigtable.php">Student Medication</a></li>
                     <li><a href="../administer/administer.html">Administer Medication</a></li>
                     <li><a href="../whole_school/whole_school.php">Whole School Medication</a></li>
-                    <li class="logout"><a>Logout</a></li>
+                    <li class="logout"><a href="../logout.php">Logout</a></li>
 
                 </ul>
 
             </div>
 
-                <form method="post">
+            <form method="post" action="">
 
-                    <table>
+                <table>
 
-                        <tr>
+                    <tr>
 
-                            <td><label for="medication">Medication Name: </label></td>
-                            <td><input type="text" id="medication" placeholder="Enter Medication Name" required></td>
+                        <td><label for="medication">Medication Name: </label></td>
+                        <td><input type="text" id="medication" name="medication" placeholder="Enter Medication Name" required></td>
+                    </tr>
 
-                        </tr>
+                </table>
 
+                <button type="submit">Submit</button>
 
-                    </table>
-
-                </form>
+            </form>
 
         </div>
 
     </body>
 
 </html>
-
-<?php
-
-    $sql = "INSERT INTO med VALUES =?";
-
-    $stmt = $conn->prepare($sql);
-    $stmt -> bindParam(1, $_POST['medication']);
-    $stmt -> execute();
-
-?>
