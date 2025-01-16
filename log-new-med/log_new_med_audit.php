73521c963669e9cdef5a285a0cff95d89f2f0fa0
alexbaxter07 <alexbaxter07@gmail.com>
2025-01-16 12:36:50 +0000

    updated: inserts into nav bar

--- log-new-med/log_new_med_audit.php
+++ log-new-med/log_new_med_audit.php
@@ -5,4 +5,23 @@
     // Include the database connection file
     include "../server/db_connect.php";
 
+    $sid = $_POST['student_id'];
+    $max_dose = $_POST['max_dose'];
+    $min_dose = $_POST['min_dose'];
+    $expiry = $_POST['expiry'];
+    $med = $_POST['meds'];
+    $brand = $_POST['brand'];
+    $strength = $_POST['strength'];
+
+    $sql = "INSERT into takes VALUES (?,?,?,?,?,?,?)"
+
+    $stmt = $conn->prepare($sql);
+    $stmt ->bindParam(1, $sid, PDO::PARAM_STR);
+    $stmt ->bindParam(2, $max_dose, PDO::PARAM_STR);
+    $stmt ->bindParam(3, $min_dose, PDO::PARAM_STR);
+    $stmt ->bindParam(4, $expiry, PDO::PARAM_STR);
+    $stmt ->bindParam(5, $med, PDO::PARAM_STR);
+    $stmt ->bindParam(6, $brand, PDO::PARAM_STR);
+    $stmt ->bindParam(7, $strength, PDO::PARAM_STR);
+    $stmt ->execute();
 ?>
