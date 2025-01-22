4aa240c22f7c9855ebccdf3949cfdab4e9f77998
James <barowikajames@gmail.com>
2025-01-22 19:27:16 +0000

    updated: database schema

--- database/database.sql
+++ database/database.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 20, 2025 at 01:04 AM
--- Server version: 8.0.40-cll-lve
--- PHP Version: 8.3.14
+-- Generation Time: Jan 22, 2025 at 07:25 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,10 +18,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `barowika_utcl-mts`
+-- Database: `utcl-mts`
 --
-CREATE DATABASE IF NOT EXISTS `utcl-mts` DEFAULT CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci;
-USE `utcl-mts`;
 
 -- --------------------------------------------------------
 
@@ -32,7 +30,7 @@ USE `utcl-mts`;
 CREATE TABLE `administer` (
   `adminster_id` int NOT NULL,
   `takes_id` int NOT NULL,
-  `staff_code` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `staff_code` text COLLATE utf8mb4_general_ci NOT NULL,
   `date_time` int NOT NULL,
   `dose_given` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -46,7 +44,7 @@ CREATE TABLE `administer` (
 CREATE TABLE `audit_logs` (
   `audit_id` int UNSIGNED NOT NULL,
   `staff_id` int NOT NULL,
-  `act` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `act` text COLLATE utf8mb4_general_ci NOT NULL,
   `date_time` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -58,7 +56,7 @@ CREATE TABLE `audit_logs` (
 
 CREATE TABLE `brand` (
   `brand_id` int UNSIGNED NOT NULL,
-  `brand_name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+  `brand_name` text COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -101,7 +99,7 @@ INSERT INTO `brand` (`brand_id`, `brand_name`) VALUES
 (33, 'VIBRAMYCIN'),
 (34, 'DELTASONE'),
 (35, 'PLAVIX'),
-(36, 'PANADOL');
+(36, 'PANADOL'),
 
 --
 -- Triggers `brand`
@@ -122,26 +120,12 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `log`
---
-
-CREATE TABLE `log` (
-  `log_id` int NOT NULL,
-  `student_id` int NOT NULL,
-  `staff_id` int NOT NULL,
-  `notes` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `date_time` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `med`
 --
 
 CREATE TABLE `med` (
   `med_id` int UNSIGNED NOT NULL,
-  `med_name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+  `med_name` text COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -202,25 +186,50 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `notes`
+--
+
+CREATE TABLE `notes` (
+  `note_id` int NOT NULL,
+  `takes_id` int NOT NULL,
+  `staff_code` text COLLATE utf8mb4_general_ci NOT NULL,
+  `content` text COLLATE utf8mb4_general_ci NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `notes`
+--
+
+INSERT INTO `notes` (`note_id`, `takes_id`, `staff_code`, `content`, `created_at`) VALUES
+(1, 103, '', 'Testing Info', '2025-01-21 23:47:23'),
+(2, 103, '', 'asdasdasd', '2025-01-02 01:50:00'),
+(3, 103, 'USU', 'Testing with staff_code', '2025-01-23 03:58:00'),
+(4, 103, 'USU', 'asdasdasd', '2025-01-27 03:09:00');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `staff`
 --
 
 CREATE TABLE `staff` (
   `staff_id` int UNSIGNED NOT NULL,
-  `first_name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `last_name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `email` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `password` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `group` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+  `first_name` text COLLATE utf8mb4_general_ci NOT NULL,
+  `last_name` text COLLATE utf8mb4_general_ci NOT NULL,
+  `email` text COLLATE utf8mb4_general_ci NOT NULL,
+  `staff_code` text COLLATE utf8mb4_general_ci NOT NULL,
+  `password` text COLLATE utf8mb4_general_ci NOT NULL,
+  `group` text COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `staff`
 --
 
-INSERT INTO `staff` (`staff_id`, `first_name`, `last_name`, `email`, `password`, `group`) VALUES
-(1, 'Failed', 'Events', 'failed.events@utcleeds.co.uk', 'failed.events', 'system'),
-(5, 'user', 'user', 'user.user@utcleeds.co.uk', '$2y$10$vF4bf/M/qEBKJXKLCwI/AeJPjw7ELS8MyQ9zu3cHRTno55kWX7PeK', 'admin');
+INSERT INTO `staff` (`staff_id`, `first_name`, `last_name`, `email`, `staff_code`, `password`, `group`) VALUES
+(1, 'Failed', 'Events', 'failed.events@utcleeds.co.uk', '', 'failed.events', 'system'),
+(5, 'user', 'user', 'user.user@utcleeds.co.uk', 'UUS', '$2y$10$vF4bf/M/qEBKJXKLCwI/AeJPjw7ELS8MyQ9zu3cHRTno55kWX7PeK', 'admin');
 
 -- --------------------------------------------------------
 
@@ -230,9 +239,9 @@ INSERT INTO `staff` (`staff_id`, `first_name`, `last_name`, `email`, `password`,
 
 CREATE TABLE `students` (
   `student_id` int UNSIGNED NOT NULL,
-  `first_name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `last_name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `year` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+  `first_name` text COLLATE utf8mb4_general_ci NOT NULL,
+  `last_name` text COLLATE utf8mb4_general_ci NOT NULL,
+  `year` text COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -241,38 +250,38 @@ CREATE TABLE `students` (
 
 INSERT INTO `students` (`student_id`, `first_name`, `last_name`, `year`) VALUES
 (1, 'JAKE', 'PERALTA', '13'),
-(2, 'AMY', 'SANTIAGO', '11'),
+(2, 'TEST', 'CHANGES', '11'),
 (3, 'CHARLES', 'BOYLE', '10'),
-(4, 'ROSA', 'DIAZ', '12'),
+(4, 'ROSA', 'DIAZ', '14'),
 (5, 'GINA', 'LINETTI', '13'),
 (6, 'RAYMOND', 'HOLT', '13'),
-(7, 'LUCY', 'CHEN', '12'),
+(7, 'LUCY', 'CHEN', '14'),
 (8, 'TIM', 'BRADFORD', '11'),
 (9, 'JOHN', 'NOLAN', '13'),
-(10, 'JACKSON', 'WEST', '12'),
+(10, 'JACKSON', 'WEST', '14'),
 (11, 'BAILEY', 'NUNE', '10'),
 (12, 'WESLEY', 'EVENS', '10'),
 (13, 'ANGELA', 'LOPEZ', '11'),
-(14, 'ROSALIND', 'DYER', '12'),
+(14, 'ROSALIND', 'DYER', '14'),
 (15, 'NYLA', 'HARPER', '13'),
 (16, 'AARON', 'THORSEN', '11'),
 (17, 'TAILIA', 'BISHOP', '11'),
-(18, 'ZOE', 'ANDERSON', '12'),
+(18, 'ZOE', 'ANDERSON', '14'),
 (19, 'TAMARA', 'COLLINS', '10'),
 (20, 'CELINA', 'JUAREZ', '10'),
-(21, 'ELIJA', 'STONE', '12'),
+(21, 'ELIJA', 'STONE', '14'),
 (22, 'MONICA', 'STEVENS', '13'),
-(23, 'OSCAR', 'HUTCHINSON', '12'),
-(24, 'WADE', 'GREY', '12'),
+(23, 'OSCAR', 'HUTCHINSON', '14'),
+(24, 'WADE', 'GREY', '14'),
 (25, 'EMMET', 'LANG', '13'),
 (26, 'JAMES', 'MURRAY', '13'),
 (27, 'MEREDITH', 'GREY', '11'),
-(28, 'DEREK', 'SHEPARD', '12'),
+(28, 'DEREK', 'SHEPARD', '14'),
 (29, 'ALEX', 'KEREV', '10'),
 (30, 'MARK', 'SLOAN', '11'),
 (31, 'OWEN', 'HUNT', '13'),
 (32, 'CRISTINA', 'YANG', '11'),
-(33, 'MIRANDA', 'BAILEY', '12');
+(33, 'MIRANDA', 'BAILEY', '14'),
 
 --
 -- Triggers `students`
@@ -307,7 +316,7 @@ CREATE TABLE `takes` (
   `current_dose` int NOT NULL,
   `min_dose` int NOT NULL,
   `max_dose` int NOT NULL,
-  `strength` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `strength` text COLLATE utf8mb4_general_ci NOT NULL,
   `notes` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
   `archived` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -317,7 +326,7 @@ CREATE TABLE `takes` (
 --
 
 INSERT INTO `takes` (`takes_id`, `student_id`, `med_id`, `brand_id`, `exp_date`, `current_dose`, `min_dose`, `max_dose`, `strength`, `notes`, `archived`) VALUES
-(103, 1, 1, 1, 1735862400, 2, 4, 8, '100mg', '', 1),
+(103, 1, 1, 1, 1735862400, 0, 4, 8, '100mg', '', 1),
 (104, 2, 2, 2, 1707004800, 4, 1, 30, '50mg', '', 1),
 (105, 2, 3, 2, 1733702400, 32, 6, 32, '200mg', '', 0),
 (106, 3, 3, 4, 1760659200, 6, 6, 49, '200 mg', '', 0),
@@ -333,7 +342,7 @@ INSERT INTO `takes` (`takes_id`, `student_id`, `med_id`, `brand_id`, `exp_date`,
 (116, 6, 13, 13, 1693526400, 21, 3, 60, '200mg', '', 1),
 (117, 7, 14, 14, 1751328000, 1, 2, 1, '0.3mg', '', 0),
 (118, 7, 14, 14, 1751328000, 1, 8, 1, '0.3mg', '', 0),
-(119, 8, 15, 15, 1734134400, 5, 9, 7, '30g', 'Stored in a fridge', 0),
+(119, 8, 15, 15, 1734134400, 5, 9, 7, '30g', '', 0),
 (120, 8, 20, 16, 1769904000, 40, 10, 60, '125mg', '', 0),
 (121, 9, 1, 17, 1759276800, 4, 6, 8, '500mg', '', 0),
 (122, 9, 18, 18, 1732838400, 6, 2, 7, '30mg', '', 1),
@@ -359,13 +368,17 @@ INSERT INTO `takes` (`takes_id`, `student_id`, `med_id`, `brand_id`, `exp_date`,
 (142, 26, 24, 28, 1696550400, 10, 6, 30, '40 mg', '', 1),
 (143, 27, 25, 29, 1821312000, 60, 9, 100, '81 mg', '', 0),
 (144, 27, 26, 30, 1775952000, 15, 3, 20, '1 mg', '', 0),
-(145, 28, 27, 31, 1669507200, 35, 3, 50, '5 mg', 'uasihdajsdkjasdasdjasojasd', 0),
+(145, 28, 27, 31, 1669507200, 35, 3, 50, '5 mg', '', 0),
 (146, 28, 28, 32, 1707091200, 12, 2, 30, '300 mg', '', 0),
-(147, 29, 29, 33, 1732406400, 20, 7, 30, '100 mg', 'asdfg', 0),
+(147, 29, 29, 33, 1732406400, 20, 7, 30, '100 mg', '', 0),
 (148, 29, 12, 20, 1696723200, 40, 10, 50, '10 mg', '', 0),
 (149, 30, 7, 34, 1800835200, 10, 4, 20, '5 mg', '', 0),
 (150, 31, 30, 35, 1710374400, 60, 10, 90, '75 mg', '', 0),
-(151, 31, 1, 36, 1755734400, 20, 2, 30, '500 mg', '', 0);
+(151, 31, 1, 36, 1755734400, 20, 2, 30, '500 mg', '', 0),
+(152, 20, 5, 4, 1747267200, 69, 69, 420, '69', NULL, 0),
+(153, 20, 5, 4, 1747267200, 69, 69, 420, '69', NULL, 0),
+(154, 1, 1, 2, 1737504000, 12, 12, 12, '50', NULL, 0),
+(155, 1, 2, 2, 1747958400, 12, 12, 12, '12', NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -376,7 +389,7 @@ INSERT INTO `takes` (`takes_id`, `student_id`, `med_id`, `brand_id`, `exp_date`,
 CREATE TABLE `whole_log` (
   `whole_log_id` int NOT NULL,
   `whole_school_id` int NOT NULL,
-  `notes` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `notes` text COLLATE utf8mb4_general_ci NOT NULL,
   `date_time` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -388,10 +401,10 @@ CREATE TABLE `whole_log` (
 
 CREATE TABLE `whole_school` (
   `whole_school_id` int NOT NULL,
-  `name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `name` text COLLATE utf8mb4_general_ci NOT NULL,
   `exp_date` int NOT NULL,
   `amount_left` int NOT NULL,
-  `notes` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `notes` text COLLATE utf8mb4_general_ci NOT NULL,
   `archived` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -425,16 +438,16 @@ ALTER TABLE `brand`
   ADD PRIMARY KEY (`brand_id`);
 
 --
--- Indexes for table `log`
---
-ALTER TABLE `log`
-  ADD PRIMARY KEY (`log_id`);
-
---
 -- Indexes for table `med`
 --
 ALTER TABLE `med`
   ADD PRIMARY KEY (`med_id`);
+
+--
+-- Indexes for table `notes`
+--
+ALTER TABLE `notes`
+  ADD PRIMARY KEY (`note_id`);
 
 --
 -- Indexes for table `staff`
@@ -480,7 +493,7 @@ ALTER TABLE `administer`
 -- AUTO_INCREMENT for table `audit_logs`
 --
 ALTER TABLE `audit_logs`
-  MODIFY `audit_id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
+  MODIFY `audit_id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=114;
 
 --
 -- AUTO_INCREMENT for table `brand`
@@ -489,16 +502,16 @@ ALTER TABLE `brand`
   MODIFY `brand_id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
 
 --
--- AUTO_INCREMENT for table `log`
---
-ALTER TABLE `log`
-  MODIFY `log_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
 -- AUTO_INCREMENT for table `med`
 --
 ALTER TABLE `med`
   MODIFY `med_id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
+-- AUTO_INCREMENT for table `notes`
+--
+ALTER TABLE `notes`
+  MODIFY `note_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `staff`
@@ -510,13 +523,13 @@ ALTER TABLE `staff`
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `student_id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=877;
+  MODIFY `student_id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=917;
 
 --
 -- AUTO_INCREMENT for table `takes`
 --
 ALTER TABLE `takes`
-  MODIFY `takes_id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=154;
+  MODIFY `takes_id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=156;
 
 --
 -- AUTO_INCREMENT for table `whole_log`
