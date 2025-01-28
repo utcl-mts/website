8c7aeb53e0f20cf10e8ce0582a0f6c9dcbbee8ed
James <barowikajames@gmail.com>
2025-01-28 19:18:36 +0000

    fixed: typo

--- admin/archived_staff.php
+++ admin/archived_staff.php
@@ -13,7 +13,7 @@ include "../server/navbar/admin_dashboard.php";
 // Fetch staff data
 $query = "SELECT staff_id, first_name, last_name, email, staff_code 
           FROM staff 
-          WHERE staff_id != 1 AND archived != 1";
+          WHERE staff_id != 1 AND archived != 0";
 $stmt = $conn->prepare($query);
 $stmt->execute();
 $staffData = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -31,13 +31,13 @@ $staffData = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <div>
     <ul class="nav_bar">
         <div class="nav_left">
-            <li class="navbar_li"><a class='active' href="staff_home.php">View All Staff</a></li>
-            <li class="navbar_li"><a href="archived_staff.php">Archived Staff</a></li>
+            <li class="navbar_li"><a href="staff_home.php">View All Staff</a></li>
+            <li class="navbar_li"><a class='active' href="archived_staff.php">Archived Staff</a></li>
             <li class="navbar_li"><a href="create_user_form.php">Create new staff</a></li>
         </div>
     </ul>
 </div>
-<h1>Staff Management</h1>
+<h1>Archived Staff</h1>
 <table class="big_table">
     <thead>
     <tr>
@@ -46,7 +46,6 @@ $staffData = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <th class="big_table_th">Last Name</th>
         <th class="big_table_th">Email</th>
         <th class="big_table_th">Staff Code</th>
-        <th class="big_table_th">Actions</th>
     </tr>
     </thead>
     <tbody>
@@ -58,21 +57,6 @@ $staffData = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 <td class="big_table_td"><?= htmlspecialchars($row['last_name']) ?></td>
                 <td class="big_table_td"><?= htmlspecialchars($row['email']) ?></td>
                 <td class="big_table_td"><?= htmlspecialchars($row['staff_code']) ?></td>
-                <td class="action-buttons">
-                    <form action="edit_user.php" method="GET" style="display:inline;">
-                        <input type="hidden" name="staff_id" value="<?= htmlspecialchars($row['staff_id']) ?>">
-                        <button class="table_button" type="submit">Edit Details</button>
-                    </form>
-                    <form action="change_password.php" method="GET" style="display:inline;">
-                        <input type="hidden" name="staff_id" value="<?= htmlspecialchars($row['staff_id']) ?>">
-                        <button class="table_button" type="submit">Change Password</button>
-                    </form>
-                    <!-- <form action="delete_user.php" method="POST" style="display:inline;"> -->
-                    <form action="delete_user.php" method="post" style="display:inline;" onsubmit="return confirm('Are you sure you want to delete this member of staff?');">
-                        <input type="hidden" name="staff_id" value="<?= htmlspecialchars($row['staff_id']) ?>">
-                        <button class="table_button" type="submit">Delete</button>
-                    </form>
-                </td>
             </tr>
         <?php endforeach; ?>
     <?php else: ?>
