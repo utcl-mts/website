87c23cc6f4ff1c798c3680bd4872c00540181a17
James <barowikajames@gmail.com>
2025-02-02 21:28:56 +0000

    added: audit for dashboard

--- dashboard/archive.php
+++ dashboard/archive.php
@@ -3,6 +3,7 @@
 session_start();
 
 include "../server/db_connect.php";
+include "../server/audit-log.php";
 include "../server/check_cookie_user.php";
 
 if (isset($_POST['takes_id'])) {
@@ -11,6 +12,12 @@ if (isset($_POST['takes_id'])) {
     $sql = "UPDATE takes SET archived = 1 WHERE takes_id = :takes_id";
     $stat = $conn->prepare($sql);
     $stat->bindParam(':takes_id', $takes_id, PDO::PARAM_INT);
+
+    $staff_id = $_SESSION['staff_id'];
+    $staff_code = $_SESSION['staff_code'];
+    $action = "$staff_code Archived $takes_id";
+
+    logAction($conn, $staff_id, $action);
 
     if ($stat->execute()) {
         // Redirect back to the notification page
